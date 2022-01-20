daeb771218e319aa1851e5b7a5e81c3372650e8c
KsuVla <ksu.xa.v.ya@gmail.com>
2022-01-20 15:09:29 +0700

    added Dropdown and DropdownDates

---
+++ src/components/dropdown-dates/dropdown-dates.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './dropdown-dates.scss';
+
+type Props = {
+  data: ItemDropdown[];
+};
+type ItemDropdown = {
+  topic: string;
+  activeDate?: string;
+};
+
+class DropdownDates extends React.Component<Props> {
+  static defaultProps = {
+    topic: '',
+    activeDate: 'ДД.ММ.ГГГГ',
+  };
+
+  data: Props;
+
+  constructor(props: Props) {
+    super(props);
+    this.data = this.props;
+  }
+
+  getObj(obj: ItemDropdown) {
+    const topic = obj.topic || '';
+    const activeDate = obj.activeDate || 'ДД.ММ.ГГГГ';
+    const newObj = { topic: topic, activeDate: activeDate };
+    return newObj;
+  }
+
+  render() {
+    let {
+      data: { data },
+    } = this;
+
+    return (
+      <div className="dropdown-dates js-dropdown-dates">
+        {data.map((obj, index) => (
+          <div className="dropdown-dates__date-block" data-name="date" key={index}>
+            {this.getObj(obj).topic !== '' && <h3>{this.getObj(obj).topic}</h3>}
+            <div className="dropdown-dates__dropdown js-dropdown-dates__dropdown">
+              {this.getObj(obj).activeDate}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    //runCalendar();
+  }
+}
+
+export default DropdownDates;
