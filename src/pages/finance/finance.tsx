7da49646feb3da53c3ad991db0f5e1f4cb889412
KsuVla <ksu.xa.v.ya@gmail.com>
2022-01-28 20:13:16 +0700

    added comment

--- src/pages/finance/finance.tsx
+++ src/pages/finance/finance.tsx
@@ -8,6 +8,7 @@ class Finance extends React.Component<{}> {
       <main className="finance">
         <div className="finance__content-container">
           <div className="finance__form-search-num">
+            {/* При добавлении новой формы нужно указать name и прописать его в this.state.arrNamesForms FormFinance.tsx */}
             <div className="finance__form-finance">
               <FormFinance name="formFinance" />
             </div>
