1ea012482538656011ecfa3407388304db7c48d4
KsuVla <ksu.xa.v.ya@gmail.com>
2022-02-01 16:15:39 +0700

    deleted 'landing-page'

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,17 @@ import { basename } from './modules/functions/functions';
 
 import Layout from './components/layout/layout';
 import StartPage from './pages/start-page/start-page';
-import LandingPage from './pages/landing-page/landing-page';
 import Finance from './pages/finance/finance';
 
 class App extends Component<{}> {
   render() {
     const startPage = <StartPage />;
-    const landingPage = <LandingPage />;
     const finance = <Finance />;
 
     return (
       <Router basename={basename}>
         <Routes>
           <Route path="/" element={<Layout header={0} content={startPage} footer={0} />} />
-          <Route path="/landing-page" element={<Layout content={landingPage} />} />
           <Route path="/finance" element={<Layout header={0} content={finance} footer={0} />} />
         </Routes>
       </Router>
