c4a05d01dd5198da46379f542791e6ea15088189
KsuVla <ksu.xa.v.ya@gmail.com>
2022-02-03 18:48:40 +0700

    fixed StartPage

--- src/pages/start-page/start-page.tsx
+++ src/pages/start-page/start-page.tsx
@@ -2,15 +2,32 @@ import React from 'react';
 import './start-page.scss';
 import Link from '../../components/link/link';
 
-class StartPage extends React.Component {
+interface Props {
+  dirs: string[];
+}
+
+class StartPage extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      arrDirMonth: [],
+    };
+  }
   render() {
     return (
       <div>
         <div className="start-page">
           <h2>Pages Toxin:</h2>
           <div className="start-page__links">
-            <Link classBlock="start-page__link" text="landing page" url={`/landing-page`} />
             <Link classBlock="start-page__link" text="finance" url={`/finance`} />
+            {this.props.dirs.map((item, index) => (
+              <Link
+                classBlock="start-page__link"
+                text={`finance ${item}`}
+                url={`/finance${item}`}
+                key={`sp-link-${index}`}
+              />
+            ))}
           </div>
         </div>
       </div>
